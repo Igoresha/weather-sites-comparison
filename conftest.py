7d759c35938cc932b4c82e5fcedc1c3f2a06cf83
Igoresha <igor.gudimof@gmail.com>
2020-02-25 21:19:46 +0200

    add chrome_options and Wait functions

--- conftest.py
+++ conftest.py
@@ -4,6 +4,10 @@ from selenium import webdriver
 
 @pytest.fixture(scope="session")
 def browser():
-    driver = webdriver.Chrome(executable_path="./drivers/chromedriver.exe")
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--start-maximized")
+    driver = webdriver.Chrome(executable_path="./drivers/chromedriver.exe", options=chrome_options)
+    driver.implicitly_wait(10)
+    driver.set_page_load_timeout(10)
     yield driver
     driver.quit()
