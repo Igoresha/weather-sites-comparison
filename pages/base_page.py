f8344e95fb8ed7b39d41bcf6f12032e4684fc883
Igoresha <igor.gudimof@gmail.com>
2020-02-25 21:17:36 +0200

    add new method for print results to BasePage class

--- pages/base_page.py
+++ pages/base_page.py
@@ -14,3 +14,9 @@ class BasePage:
 
     def go_to_site(self):
         return self.driver.get(self.base_url)
+
+    def print_forecast_results(self, city_name, date, sinoptik_temp, dark_sky_temp):
+        print("Sinoptik, city - {0}, date - {1}, forecast temp - min = {2}, max = {3}".
+              format(city_name, date, str(sinoptik_temp[0]), str(sinoptik_temp[1])))
+        print("Dark Sky, city - {0}, date - {1}, forecast temp - min = {2}, max = {3}".
+              format(city_name, date, str(dark_sky_temp[0]), str(dark_sky_temp[1])))
